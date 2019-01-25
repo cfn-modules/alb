08ebe524b480f9398172106f82ed98735e346318
Michael Wittig <michael@widdix.de>
2019-01-25 13:29:08 +0100

    add access log test

--- test/test.js
+++ test/test.js
@@ -11,3 +11,14 @@ test('defaults', async t => {
     t.pass();
   }
 });
+
+test('access-log', async t => {
+  const stackName = cfntest.stackName();
+  try {
+    t.log(await cfntest.createStack(`${__dirname}/access-log.yml`, stackName, {}));
+    // what could we test here?
+  } finally {
+    t.log(await cfntest.deleteStack(stackName));
+    t.pass();
+  }
+});
